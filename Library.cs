ab86c4c0a1963781a704ad45cfecca80156009a3
Logan Brown <logan.brown@tylertech.com>
2023-12-12 21:16:34 -0500

    Fixed missing )

--- Library.cs
+++ Library.cs
@@ -3,7 +3,7 @@ using Lethal_Library;
 using GameNetcodeStuff;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-[assembly: MelonInfo(typeof(Library, "Lethal Company Mod Library", "1.0.0", "Lillious")]
+[assembly: MelonInfo(typeof(Library), "Lethal Company Mod Library", "1.0.0", "Lillious")]
 [assembly: MelonGame("ZeekerssRBLX", "Lethal Company")]
 
 namespace Lethal_Library
